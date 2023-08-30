8123c642aea500739532b73425abd0369247dce4
itsmeYO92 <ay.aboutarbouch@gmail.com>
2023-08-30 11:35:45 +0100

    create file

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,7 @@
+#this puppet script creates a file in /tmp directory
+file { '/tmp/school':
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet',
+}
